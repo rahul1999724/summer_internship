4535e6130accfd3b194747bc4fcbd6cb22bfb823
rahul1999724 <64097707+rahul1999724@users.noreply.github.com>
2021-05-26 22:01:42 +0530

    Update output.py

--- Task1/output.py
+++ Task1/output.py
@@ -1,7 +1,7 @@
 import joblib
 name = input('Enter your name : ')
-num = float(input('Enter year of experience : '))
-model = joblib.load('marks.pk1')
+exp = float(input('Enter year of experience : '))
+model = joblib.load('Salary.pk1')
 print()
 print('Name               : ' ,name)
 print('Year_of_Experience : ', exp)
